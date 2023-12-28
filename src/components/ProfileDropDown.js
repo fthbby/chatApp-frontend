8c64d9d9f80644cbf3212f9a818418287a46bab7
Nat <fthbby@gmail.com>
2023-12-28 14:07:51 -0800

    trying to resolve 0 error

--- src/components/ProfileDropDown.js
+++ src/components/ProfileDropDown.js
@@ -16,7 +16,7 @@ import SettingsModal from "./settings/SettingsModal";
 import { userAtom } from "../stateManagement/userAtom";
 import { useRecoilState } from "recoil";
 
-export default function BasicMenu({  }) {
+export default function BasicMenu({}) {
   const navigate = useNavigate();
   const [user, setUser] = useRecoilState(userAtom);
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -42,88 +42,90 @@ export default function BasicMenu({  }) {
     <div>
       <SettingsModal open={openModal} onClose={() => setOpenModal(false)} />
 
-      <>
-        <Box
-          sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
-        >
-          <Tooltip title="Account settings">
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? "account-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            {user.image ? (
+              <Avatar
+                sx={{ borderRadius: 25, width: 45, height: 45 }}
+                src={user.image}
+              />
+            ) : (
+              <Avatar sx={{ borderRadius: 25, width: 45, height: 45 }}>
+                {user?.firstName[0].toUpperCase()}
+                {user?.lastName[0].toUpperCase()}
+              </Avatar>
+            )}
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Box display={"flex"}>
+            {user?.image.length > 0 ? (
+              <Avatar
+                sx={{ borderRadius: 25, width: 70, height: 70, mr: 2 }}
+                src={user?.image}
+              />
+            ) : (
+              <Avatar
+                sx={{
+                  borderRadius: 25,
+                  width: 70,
+                  height: 70,
+                  fontSize: 30,
+                  mr: 2,
+                }}
+              >
+                {user?.firstName[0]?.toUpperCase()}
+                {user?.lastName[0]?.toUpperCase()}
+              </Avatar>
+            )}
+
+            <Box
+              display="flex"
+              flexDirection={"column"}
+              justifyContent={"center"}
             >
-              {user.image ? (
-                <Avatar
-                  sx={{ borderRadius: 25, width: 45, height: 45 }}
-                  src={user.image}
-                />
-              ) : (
-                <Avatar sx={{ borderRadius: 25, width: 45, height: 45 }}>
-                  {user?.firstName[0].toUpperCase()}
-                  {user?.lastName[0].toUpperCase()}
-                </Avatar>
-              )}
-            </IconButton>
-          </Tooltip>
-        </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-          <MenuItem onClick={handleClose}>
-            <Box display={"flex"}>
-              {user?.image ? (
-                <Avatar
-                  sx={{ borderRadius: 25, width: 70, height: 70, mr: 2 }}
-                  src={user?.image}
-                />
-              ) : (
-                <Avatar
-                  sx={{
-                    borderRadius: 25,
-                    width: 70,
-                    height: 70,
-                    fontSize: 30,
-                    mr: 2,
-                  }}
-                >
-                  {user?.firstName[0]?.toUpperCase()}
-                  {user?.lastName[0]?.toUpperCase()}
-                </Avatar>
-              )}
-
-              <Box display="flex" flexDirection={"column"} justifyContent={'center'}>
-                <Typography>{user?.firstName} {user?.lastName}</Typography>
-                <Typography color="gray">{user?.email}</Typography>
-                <Typography>Available</Typography>
-              </Box>
+              <Typography>
+                {user?.firstName} {user?.lastName}
+              </Typography>
+              <Typography color="gray">{user?.email}</Typography>
+              <Typography>Available</Typography>
             </Box>
-          </MenuItem>
+          </Box>
+        </MenuItem>
 
-          <Divider />
+        <Divider />
 
-          <MenuItem onClick={onManageAccount}>
-            <ListItemIcon>
-              <ContactMailIcon fontSize="small" />
-            </ListItemIcon>
-            Manage Account
-          </MenuItem>
-          <Divider />
+        <MenuItem onClick={onManageAccount}>
+          <ListItemIcon>
+            <ContactMailIcon fontSize="small" />
+          </ListItemIcon>
+          Manage Account
+        </MenuItem>
+        <Divider />
 
-          <MenuItem onClick={handleLogOut}>
-            <ListItemIcon>{/* <Logout fontSize="small" /> */}</ListItemIcon>
-            Sign Out
-          </MenuItem>
-        </Menu>
-      </>
+        <MenuItem onClick={handleLogOut}>
+          <ListItemIcon>{/* <Logout fontSize="small" /> */}</ListItemIcon>
+          Sign Out
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
